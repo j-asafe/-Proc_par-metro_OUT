b8268d849cb285d381d9f374f4fbc49b5dda9ce6
asafe <joao.soares203@etec.sp.gov.br>
2025-08-26 22:16:30 -0300

    Delete Escola_BD directory

--- Escola_BD/call.sql
+++
@@ -1,3 +0,0 @@
-CALL InserirDadosAluno('Maria Silva', 17, '3DS', 'técnico em desenvolvimento de sistemas', 9.5);
-CALL InserirDadosAluno('João Souza', 18, '3DS', 'técnico em desenvolvimento de sistemas', 8.0);
-CALL InserirDadosAluno('Ana Paula', 17, '3DS', 'técnico em desenvolvimento de sistemas', 7.8);
